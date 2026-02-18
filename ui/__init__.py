ff40821ce71f634f81d8f0ee26fc1ef0a24d6374
josecruzp-enee <j90nikol@gmail.com>
2026-02-18 13:13:46 -0600

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,7 @@
 #__init__.py
+from . import datos_cliente
+from . import consumo_energetico
+from . import sistema_fv
+from . import seleccion_equipos
+from . import ingenieria_electrica
+from . import resultados 
