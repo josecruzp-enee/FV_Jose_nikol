635b478e3beed0401a18fefd3d783f06ba7242a0
josecruzp-enee <j90nikol@gmail.com>
2026-02-18 08:08:37 -0600

    Update simular_12_meses.py

--- core/simular_12_meses.py
+++ core/simular_12_meses.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import Dict, List
 
-from nucleo.modelo import DatosProyecto
+from nucleo.modelo import modelo
 
 
 def consumo_anual(consumo_12m: List[float]) -> float:
