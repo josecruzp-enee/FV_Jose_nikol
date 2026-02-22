14fd599bf3794b73bc7e0d621cf66a2891d2a4f1
josecruzp-enee <j90nikol@gmail.com>
2026-02-22 10:52:01 -0600

    Update paquete_electrico.py

--- electrical/ref/paquete_electrico.py
+++ electrical/ref/paquete_electrico.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 from typing import Any, Dict, List, Optional
 
-from electrical.modelos import ParametrosCableado
+from electrical.catalogos.modelos import ParametrosCableado
 from electrical.calculos_regulacion import tramo_dc_ref, tramo_ac_1f_ref, tramo_ac_3f_ref
 from electrical.protecciones import armar_ocpd
 from electrical.canalizacion import conduit_ac_heuristico, canalizacion_fv
