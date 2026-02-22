156726845b0a94f951a149ff742f19e8d3d6084b
josecruzp-enee <j90nikol@gmail.com>
2026-02-22 10:45:49 -0600

    Update __init__.py

--- electrical/catalogos/__init__.py
+++ electrical/catalogos/__init__.py
@@ -1,1 +1,17 @@
+from .catalogos import (
+    get_panel,
+    get_inversor,
+    ids_paneles,
+    ids_inversores,
+    catalogo_paneles,
+    catalogo_inversores,
+)
 
+__all__ = [
+    "get_panel",
+    "get_inversor",
+    "ids_paneles",
+    "ids_inversores",
+    "catalogo_paneles",
+    "catalogo_inversores",
+]
