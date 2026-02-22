f41029db6319e2ada90f06a30ddf7caf4bf2c0cf
josecruzp-enee <j90nikol@gmail.com>
2026-02-22 10:05:32 -0600

    Delete electrical/paquete_ref.py

--- electrical/paquete_ref.py
+++
@@ -1,5 +0,0 @@
-"""Shim de compatibilidad para import legacy ``electrical.paquete_ref``."""
-
-from .paquete_electrico import calcular_paquete_electrico_ref
-
-__all__ = ["calcular_paquete_electrico_ref"]
