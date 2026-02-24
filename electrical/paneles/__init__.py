f2a8681895c58eb63734f6c1bc4a6e3032999adc
josecruzp-enee <j90nikol@gmail.com>
2026-02-23 21:43:50 -0600

    Update __init__.py

--- electrical/paneles/__init__.py
+++ electrical/paneles/__init__.py
@@ -1,3 +1,8 @@
-from .strings_auto import calcular_strings_fv
+from .calculo_de_strings import (
+    calcular_strings_fv,
+    calcular_strings_auto,
+    PanelSpec,
+    InversorSpec,
+)
 
 __all__ = ["calcular_strings_fv"]
