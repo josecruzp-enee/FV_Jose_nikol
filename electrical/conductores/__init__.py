33c54bf3f4563d072f04f50b4f2f60efa03d77b5
josecruzp-enee <j90nikol@gmail.com>
2026-02-27 14:37:31 -0600

    Update __init__.py

--- electrical/conductores/__init__.py
+++ electrical/conductores/__init__.py
@@ -7,8 +7,8 @@ API pública:
 - caida_tension_pct
 """
 
-from .corrientes import calcular_corrientes
 from .calculo_conductores import tramo_conductor
+from .corrientes import calcular_corrientes
 from .modelo_tramo import caida_tension_pct
 
 __all__ = ["calcular_corrientes", "tramo_conductor", "caida_tension_pct"]
