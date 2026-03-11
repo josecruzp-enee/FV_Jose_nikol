65cdd8b4cadffef9d8df101a9957f05532cab859
josecruzp-enee <j90nikol@gmail.com>
2026-03-11 17:42:07 -0600

    Update circuitos_dc.py

--- electrical/dominio/circuitos_dc.py
+++ electrical/dominio/circuitos_dc.py
@@ -1,1 +1,14 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class MPPTCircuit:
+
+    id_mppt: int
+    strings: int
+
+    imp_string: float
+    isc_string: float
+
+    corriente_operacion: float
+    corriente_diseno: float
