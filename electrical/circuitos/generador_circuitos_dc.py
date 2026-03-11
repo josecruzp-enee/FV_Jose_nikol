45b75f256294edd21f8f1fb97bedae9a17286fc0
josecruzp-enee <j90nikol@gmail.com>
2026-03-11 17:40:04 -0600

    Create generador_circuitos_dc.py

---
+++ electrical/circuitos/generador_circuitos_dc.py
@@ -0,0 +1,19 @@
+def crear_circuitos_mppt(strings_totales, mppts, imp, isc):
+
+    distribucion = distribuir_strings(strings_totales, mppts)
+
+    circuitos = []
+
+    for i, n in enumerate(distribucion):
+
+        i_oper = n * imp
+        i_dis = n * isc * 1.25
+
+        circuitos.append({
+            "mppt": i + 1,
+            "strings": n,
+            "i_operacion": i_oper,
+            "i_diseno": i_dis
+        })
+
+    return circuitos
