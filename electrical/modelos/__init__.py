80698c72233ad2f11019c3cd316faa493a277204
josecruzp-enee <j90nikol@gmail.com>
2026-03-12 14:19:11 -0600

    Update __init__.py

--- electrical/modelos/__init__.py
+++ electrical/modelos/__init__.py
@@ -1,1 +1,28 @@
+"""
+Modelos del dominio eléctrico FV.
 
+FRONTERA DEL PAQUETE
+====================
+
+Exporta los contratos de datos usados por el motor eléctrico.
+
+Modelos disponibles:
+    PanelSpec
+    InversorSpec
+
+Consumido por:
+    electrical.paneles
+    electrical.inversor
+    electrical.corrientes
+    electrical.protecciones
+    electrical.conductores
+    electrical.nec
+"""
+
+from .paneles import PanelSpec
+from .inversor import InversorSpec
+
+__all__ = [
+    "PanelSpec",
+    "InversorSpec",
+]
