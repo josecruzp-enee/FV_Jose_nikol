f22f38613583e7679950b77a9fdaa87cd84ddedf
josecruzp-enee <j90nikol@gmail.com>
2026-03-11 18:37:22 -0600

    Update distribucion_mppt.py

--- electrical/paneles/distribucion_mppt.py
+++ electrical/paneles/distribucion_mppt.py
@@ -15,3 +15,30 @@ def distribuir_strings(strings_totales, mppts):
         distribucion.append(n)
 
     return distribucion
+
+def crear_circuitos_mppt(strings_totales, mppts, imp):
+
+    distribucion = distribuir_strings(strings_totales, mppts)
+
+    circuitos = []
+
+    for i, n in enumerate(distribucion):
+
+        i_oper = n * imp
+
+        # NEC 690.8
+        i_dis = i_oper * 1.25
+
+        circuitos.append({
+
+            "mppt": i + 1,
+
+            "strings": n,
+
+            "i_operacion": i_oper,
+
+            "i_diseno": i_dis
+
+        })
+
+    return circuitos
