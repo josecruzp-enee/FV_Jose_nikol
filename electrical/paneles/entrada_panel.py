1df64ad84f25db7f010967d3182912a6b7f6d86a
josecruzp-enee <j90nikol@gmail.com>
2026-03-13 00:01:43 -0600

    Update entrada_panel.py

--- electrical/paneles/entrada_panel.py
+++ electrical/paneles/entrada_panel.py
@@ -47,7 +47,7 @@ class EntradaPaneles:
     # ----------------------------------------------
 
     n_paneles_total: int
-    n_inversores=sizing["n_inversores"],
+    
     # ----------------------------------------------
     # Condiciones térmicas
     # ----------------------------------------------
