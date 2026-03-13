7c0adbd12aa0a93e4d102aa14bba7ec26317c827
josecruzp-enee <j90nikol@gmail.com>
2026-03-13 01:32:27 -0600

    Update page_5.py

--- reportes/page_5.py
+++ reportes/page_5.py
@@ -5,8 +5,11 @@ from reportlab.platypus import Paragraph, Spacer, PageBreak
 from .secciones_tecnicas.resumen_tecnico import build_resumen_tecnico
 from .secciones_tecnicas.tabla_strings import crear_tabla_strings
 from .secciones_tecnicas.tabla_distribucion_strings import crear_tabla_distribucion_inversores
-from .secciones_tecnicas.tabla_nec import crear_tabla_parametros_electricos
-from .secciones_tecnicas.tabla_nec import crear_tabla_diseno_nec
+from .secciones_tecnicas.tabla_nec import (
+    crear_tabla_parametros_electricos,
+    crear_tabla_dimensionamiento_nec,
+    crear_tabla_indicadores
+)
 from .secciones_tecnicas.layout_paneles import insertar_layout_paneles
 
 
