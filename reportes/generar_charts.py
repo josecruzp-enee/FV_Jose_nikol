ee87175d883dfbfb64e23dcddba90f4f4710a6cb
josecruzp-enee <j90nikol@gmail.com>
2026-03-13 04:04:49 -0600

    Update generar_charts.py

--- reportes/generar_charts.py
+++ reportes/generar_charts.py
@@ -170,6 +170,9 @@ def generar_charts(
     base = _mkdir_charts(out_dir)
 
     pdc_kw = _leer_pdc_kw(res)
+    import streamlit as st
+
+    st.write("DEBUG ResultadoProyecto:", res)
     st.write("DEBUG PDC KW:", pdc_kw)
     meses = [
         "Ene","Feb","Mar","Abr","May","Jun",
