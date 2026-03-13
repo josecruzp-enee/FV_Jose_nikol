066d37b7b4c9f75a25f97312a34b4cd5a7a2df65
josecruzp-enee <j90nikol@gmail.com>
2026-03-13 11:46:48 -0600

    Update dependencias.py

--- core/aplicacion/dependencias.py
+++ core/aplicacion/dependencias.py
@@ -8,6 +8,36 @@ from electrical.energia.orquestador_energia import ejecutar_motor_energia
 from electrical.nec.orquestador_nec import ejecutar_nec
 from core.servicios.finanzas import ejecutar_finanzas
 
+from electrical.paneles.entrada_panel import EntradaPaneles
+
+
+class PanelesAdapter:
+
+    def ejecutar(self, datos, sizing):
+
+        entrada = EntradaPaneles(
+
+            panel=sizing.panel,
+
+            inversor=sizing.inversor,
+
+            n_paneles_total=sizing.n_paneles,
+
+            n_inversores=sizing.n_inversores,
+
+            t_min_c=datos.sistema_fv.get("t_min_c", 10),
+
+            t_oper_c=datos.sistema_fv.get("t_oper_c", 45),
+
+            dos_aguas=datos.sistema_fv.get("dos_aguas", False),
+
+            objetivo_dc_ac=datos.sistema_fv.get("dc_ac_ratio", 1.2),
+
+            pdc_kw_objetivo=sizing.pdc_kw
+
+        )
+
+        return ejecutar_paneles(entrada)
 
 class SizingAdapter:
     def ejecutar(self, datos):
