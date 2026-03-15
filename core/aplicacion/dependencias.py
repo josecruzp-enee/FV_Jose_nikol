e6b644c2747277b1309cfa3212e1f2eb3ded4306
josecruzp-enee <j90nikol@gmail.com>
2026-03-15 14:52:11 -0600

    Update dependencias.py

--- core/aplicacion/dependencias.py
+++ core/aplicacion/dependencias.py
@@ -1,19 +1,13 @@
-from __future__ import annotations
-
-from core.aplicacion.orquestador_estudio import DependenciasEstudio
-
 from core.servicios.sizing import calcular_sizing_unificado
+
 from electrical.paneles.orquestador_paneles import ejecutar_paneles
 from electrical.nec.orquestador_nec import ejecutar_nec
-from core.servicios.finanzas import ejecutar_finanzas
+
 from energy.orquestador_energia import ejecutar_motor_ener
 from energy.contrato import EnergiaInput
 
+from core.servicios.finanzas import ejecutar_finanzas
 from electrical.paneles.entrada_panel import EntradaPaneles
-from electrical.catalogos import get_panel, get_inversor
-
-from core.dominio.contrato import ResultadoStrings, StringInfo
-
 
 # ==========================================================
 # ADAPTER SIZING
