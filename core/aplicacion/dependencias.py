11c0d0f8fce48bbb2c1983fde88b443ca153ea31
josecruzp-enee <j90nikol@gmail.com>
2026-03-22 12:39:11 -0600

    Update dependencias.py

--- core/aplicacion/dependencias.py
+++ core/aplicacion/dependencias.py
@@ -4,6 +4,7 @@ from core.aplicacion.orquestador_estudio import DependenciasEstudio
 
 # ✔ IMPORTS REALES (LOS TUYOS)
 from electrical.paneles.orquestador_paneles import ejecutar_paneles
+from electrical.paneles.entrada_panel import EntradaPaneles
 from core.aplicacion.puertos import PuertoNEC
 from core.servicios.sizing import calcular_sizing_unificado
 
@@ -16,10 +17,18 @@ class SizingAdapter:
     def ejecutar(self, datos):
         return calcular_sizing_unificado(datos)
 
+
+
+
 class PanelesAdapter:
     def ejecutar(self, datos, sizing):
-        return ejecutar_paneles(datos, sizing)
 
+        entrada = EntradaPaneles(
+            datos=datos,
+            sizing=sizing
+        )
+
+        return ejecutar_paneles(entrada)
 
 class NECAdapter:
     def ejecutar(self, datos, sizing, paneles):
