7865bf5cfb8648fc638aeab3beebb63b9ba27c3d
josecruzp-enee <j90nikol@gmail.com>
2026-04-01 14:22:29 -0600

    Update multizona.py

--- core/aplicacion/multizona.py
+++ core/aplicacion/multizona.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from typing import List, Optional
-
+from dataclasses import replace
 # ==========================================================
 # DOMINIO / CONTRATOS
 # ==========================================================
