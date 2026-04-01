86ae80f49e2f37575a9441a775e9a5b1189b6bfe
josecruzp-enee <j90nikol@gmail.com>
2026-04-01 10:45:29 -0600

    Update orquestador_estudio.py

--- core/aplicacion/orquestador_estudio.py
+++ core/aplicacion/orquestador_estudio.py
@@ -105,7 +105,7 @@ def ejecutar_estudio(
                 return ResultadoProyecto(
                     sizing=sizing,
                     paneles=paneles,
-                    strings=None,
+                    strings=paneles.strings,
                     energia=energia,
                     electrical=electrical,
                     financiero=None,
