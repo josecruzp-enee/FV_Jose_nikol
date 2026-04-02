351a4f364a5bced19222400fdeba23d4c6d72b7d
josecruzp-enee <j90nikol@gmail.com>
2026-04-01 22:03:30 -0600

    Update tabla_distribucion_strings.py

--- reportes/secciones_tecnicas/tabla_distribucion_strings.py
+++ reportes/secciones_tecnicas/tabla_distribucion_strings.py
@@ -106,7 +106,7 @@ def build_resumen_tecnico(resultado, pal, styles, content_w):
     story.append(Paragraph("Distribución de strings por inversor", styles["Heading2"]))
     story.append(Spacer(1, 8))
 
-    tabla_inv = crear_tabla_distribucion_inversores(
+    tabla_inv = crear_tabla_distribucion_string(
         strings,
         pal,
         content_w
