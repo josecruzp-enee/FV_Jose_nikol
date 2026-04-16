b6588d04f53ee5552b11c4198f69acd58e158e30
josecruzp-enee <j90nikol@gmail.com>
2026-04-16 17:32:53 -0600

    Update __init__.py

--- reportes/bloques/__init__.py
+++ reportes/bloques/__init__.py
@@ -1,16 +1,13 @@
 from .resumen_ejecutivo import build_resumen_ejecutivo
 from .analisis_energetico import build_analisis_energetico
-
-# 🔴 temporales
-from ..page_3 import build_page_3
-from ..page_4 import build_page_4
-from ..page_5 import build_page_5
-
+from .analisis_financiero import build_analisis_financiero
+from .analisis_operativo import build_analisis_operativo
+from .ingenieria_electrica import build_ingenieria_electrica
 
 BLOQUES_REPORTE = [
     build_resumen_ejecutivo,
     build_analisis_energetico,
-    build_page_3,
-    build_page_4,
-    build_page_5,
+    build_analisis_financiero,
+    build_analisis_operativo,
+    build_ingenieria_electrica,
 ]
