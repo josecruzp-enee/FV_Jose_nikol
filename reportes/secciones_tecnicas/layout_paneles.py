5c868c8ef7865e933843b704eba7a0051c199b43
josecruzp-enee <j90nikol@gmail.com>
2026-06-12 21:45:50 -0600

    Update layout_paneles.py

--- reportes/secciones_tecnicas/layout_paneles.py
+++ reportes/secciones_tecnicas/layout_paneles.py
@@ -59,14 +59,14 @@ def insertar_layout_paneles(
 
         try:
             if safe_image:
-                img = safe_image(str(layout), max_w=content_w, max_h=950)
+                img = safe_image(str(layout), max_w=content_w, max_h=750)
                 if not img:
                     img = Image(str(layout))
             else:
                 img = Image(str(layout))
 
             max_w = content_w
-            max_h = 950
+            max_h = 750
 
             w = img.imageWidth
             h = img.imageHeight
