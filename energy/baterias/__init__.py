92588b1ed71793cd664c202df464eb933a438cd9
josecruzp-enee <j90nikol@gmail.com>
2026-06-17 14:31:42 -0600

    Update __init__.py

--- energy/baterias/__init__.py
+++ energy/baterias/__init__.py
@@ -4,10 +4,16 @@ from energy.baterias.modelos import ConfigBateria
 from energy.baterias.resultado_bateria import ResultadoBateria
 from energy.baterias.balance_bateria import simular_balance_bateria_24h
 from energy.baterias.orquestador_bateria import ejecutar_bateria
+from energy.baterias.recomendador_bateria import (
+    BateriaRecomendada,
+    calcular_bateria_recomendada,
+)
 
 __all__ = [
     "ConfigBateria",
     "ResultadoBateria",
     "simular_balance_bateria_24h",
     "ejecutar_bateria",
+    "BateriaRecomendada",
+    "calcular_bateria_recomendada",
 ]
