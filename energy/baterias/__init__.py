95fa0c2938546a516a266de55a9d594c036cbd50
josecruzp-enee <j90nikol@gmail.com>
2026-06-17 14:18:11 -0600

    Add ejecutar_bateria to __all__ exports

--- energy/baterias/__init__.py
+++ energy/baterias/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+
 from energy.baterias.modelos import ConfigBateria
 from energy.baterias.resultado_bateria import ResultadoBateria
 from energy.baterias.balance_bateria import simular_balance_bateria_24h
+from energy.baterias.orquestador_bateria import ejecutar_bateria
 
 __all__ = [
     "ConfigBateria",
     "ResultadoBateria",
     "simular_balance_bateria_24h",
+    "ejecutar_bateria",
 ]
