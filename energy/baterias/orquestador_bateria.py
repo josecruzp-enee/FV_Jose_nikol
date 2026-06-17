1b0add5191bff17164ab3d0f8f01e588e7f91a63
josecruzp-enee <j90nikol@gmail.com>
2026-06-17 15:34:16 -0600

    Update orquestador_bateria.py

--- energy/baterias/orquestador_bateria.py
+++ energy/baterias/orquestador_bateria.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from energy.baterias.modelos import ConfigBateria
 from energy.baterias.balance_bateria import simular_balance_bateria_24h
+from energy.baterias.perfiles import promediar_energia_8760_a_24h
 
 
 def ejecutar_bateria(
@@ -15,12 +16,15 @@ def ejecutar_bateria(
 
     Recibe:
         demanda_24h
-        fv_24h
+        fv_24h o fv_8760
         configuración batería
 
     Devuelve:
         ResultadoBateria
     """
+
+    if fv_24h and len(fv_24h) != 24:
+        fv_24h = promediar_energia_8760_a_24h(fv_24h)
 
     return simular_balance_bateria_24h(
         demanda_24h_kwh=demanda_24h,
