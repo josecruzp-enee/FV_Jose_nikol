fae79fb8500d0d5f8ad42dcc9f2da948d98ba15a
josecruzp-enee <j90nikol@gmail.com>
2026-06-17 14:16:26 -0600

    Update __init__.py

--- energy/baterias/__init__.py
+++ energy/baterias/__init__.py
@@ -1,1 +1,10 @@
+# -*- coding: utf-8 -*-
+from energy.baterias.modelos import ConfigBateria
+from energy.baterias.resultado_bateria import ResultadoBateria
+from energy.baterias.balance_bateria import simular_balance_bateria_24h
 
+__all__ = [
+    "ConfigBateria",
+    "ResultadoBateria",
+    "simular_balance_bateria_24h",
+]
