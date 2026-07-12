b6f658cf7d6ea56bb1e85d2c236fd079de05ea8b
josecruzp-enee <j90nikol@gmail.com>
2026-07-11 20:58:16 -0600

    Update __init__.py

--- energy/baterias/__init__.py
+++ energy/baterias/__init__.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import annotations
-
 from energy.baterias.entrada_bateria import (
     EntradaBateria,
+    construir_entrada_bateria,
 )
 from energy.baterias.orquestador_bateria import (
     ejecutar_sistema_bateria,
@@ -14,5 +12,6 @@ from energy.baterias.resultado_bateria import (
 __all__ = [
     "EntradaBateria",
     "ResultadoSistemaBateria",
+    "construir_entrada_bateria",
     "ejecutar_sistema_bateria",
 ]
